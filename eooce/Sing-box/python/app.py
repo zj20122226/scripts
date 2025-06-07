e4fb02df31282c57432f0d2cff7642fb24278ea0
zj20122226 <130367027+zj20122226@users.noreply.github.com>
2025-06-07 11:01:53 +0800

    Update app.py

--- eooce/Sing-box/python/app.py
+++ eooce/Sing-box/python/app.py
@@ -4,6 +4,14 @@ import subprocess
 import http.server
 import socketserver
 import threading
+import socket
+import logging
+
+# 配置logging
+logging.basicConfig(level=logging.INFO, # 设置日志级别
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    handlers=[logging.StreamHandler(sys.stdout)]) # 输出到stdout
+logger = logging.getLogger(__name__)
 
 PORT = int(os.environ.get('PORT') or 3000) # http port
 UUID = os.environ['UUID'] or '6877aae2-a8e7-44cc-ac29-c928eefa08e6'
@@ -11,7 +19,9 @@ UUID = os.environ['UUID'] or '6877aae2-a8e7-44cc-ac29-c928eefa08e6'
 class MyHandler(http.server.SimpleHTTPRequestHandler):
 
     def log_message(self, format, *args):
-        pass
+        # 如果需要HTTP服务器自身的请求日志，可以这样记录：
+        # logger.info(format % args)
+        pass # 当前设置为不记录HTTP服务器的请求日志
 
     def do_GET(self):
         if self.path == '/':
@@ -34,7 +44,16 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
             self.send_response(404)
             self.end_headers()
             self.wfile.write(b'Not found')
-httpd = socketserver.TCPServer(('', PORT), MyHandler)
+logger.info(f"Attempting to bind to address: {PORT}")
+httpd = socketserver.TCPServer(('', PORT), MyHandler, bind_and_activate=False)
+httpd.allow_reuse_address = True  # 允许重用地址
+try:
+    httpd.server_bind()    # 手动绑定
+    httpd.server_activate()  # 手动激活
+except OSError as e:
+    logger.error(f"Error binding or activating server on port {PORT}: {e}")
+    sys.exit(1)
+logger.info(f"Python HTTP server started on port {PORT} in a daemon thread.")
 server_thread = threading.Thread(target=httpd.serve_forever)
 server_thread.daemon = True
 server_thread.start()
@@ -44,14 +63,14 @@ shell_command = "chmod +x start.sh && ./start.sh"
 try:
     completed_process = subprocess.run(['bash', '-c', shell_command], stdout=sys.stdout, stderr=subprocess.PIPE, text=True, check=True)
 
-    print("App is running")
+    logger.info("App is running")
 
 except subprocess.CalledProcessError as e:
-    print(f"Error: {e.returncode}")
-    print("Standard Output:")
-    print(e.stdout)
-    print("Standard Error:")
-    print(e.stderr)
+    logger.info(f"Error: {e.returncode}")
+    logger.info("Standard Output:")
+    logger.info(e.stdout)
+    logger.info("Standard Error:")
+    logger.info(e.stderr)
     sys.exit(1)
 
 server_thread.join()
